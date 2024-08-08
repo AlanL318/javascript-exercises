9eec45d41c19b02b7487fc8c005bc1099298f643
Alan Liu <65645504+AlanL318@users.noreply.github.com>
2024-08-08 15:11:44 -0400

    Completed sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,12 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    let sum = 0
+    if (intOne < 0 || intTwo < 0) return "ERROR"
+    if (intOne > intTwo) [intOne, intTwo] = [intTwo, intOne]
+    if (!Number.isInteger(intOne) || !Number.isInteger(intTwo))  return "ERROR"
+    for (let i = intOne; i <= intTwo; i++) {
+        sum += i
+    }
+    return sum
 
 };
 
